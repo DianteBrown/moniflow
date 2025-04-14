d4a80d3103f0f4adcfc9a155627243eb0e79f9b5
Jaykumar-Tech <jaykumar930703@gmail.com>
2025-04-14 16:39:44 -0400

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/moniflow/',
   plugins: [reactPlugin(), tailwindcss()],
   resolve: {
     alias: {
